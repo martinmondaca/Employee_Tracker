13199c002495a0dfca8917ed08f3dd61488a3fc9
Martin M <martin7mondaca@gmail.com>
2021-03-13 21:28:07 -0700

    specified number of digits for salary in roles table

--- schema.sql
+++ schema.sql
@@ -12,9 +12,9 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY(department_id) REFERENCES department(id),
+    salary DECIMAL(15, 2) NOT NULL,
+    dept_id INT,
+    FOREIGN KEY(dept_id) REFERENCES department(id),
     PRIMARY KEY (id)
 );
 
